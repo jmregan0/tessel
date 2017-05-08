78b59cde817ceb7dc3ff73bdaf7b0c782dd95b3b
Jacob Regan <jacobmregan0@gmail.com>
2017-05-08 15:59:35 -0500

    Voice activation in progress

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var servo = require('./servo/servo.js');
 //var jQuery = require('jquery');
 var servo1 = 1; // We have a servo plugged in at position 1
 var readTemp = true;
+var player = require('play-sound')(opts = {})
+
 
   servo.on('motorOn', function () {
     var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
@@ -15,15 +17,18 @@ var readTemp = true;
     //  Moving them towards each other = less movement range
     //  Moving them apart = more range, more likely to stall and burn out
     servo.configure(servo1, 0.05, 0.12, function () {
+      player.play('bryan.mp3', function(err){
+        if (err) throw err
+      })
       setInterval(function () {
         console.log('Position (in range 0-1):', position);
         //  Set servo #1 to position pos.
         servo.move(servo1, position);
-
         // Increment by 10% (~18 deg for a normal servo)
         position += 0.1;
-        if (position > 1) {
+        if (position > 0.7) {
           position = 0; // Reset servo position
+
         }
       }, 500); // Every 500 milliseconds
     });
@@ -40,7 +45,7 @@ climate.on('ready', function () {
   setImmediate(function loop () {
     climate.readTemperature('f', function (err, temp) {
       climate.readHumidity(function (err, humid) {
-        if (+humid.toFixed(2) > 20.00 && readTemp){
+        if (+humid.toFixed(2) > 24.00 && readTemp){
           servo.emit('motorOn')
           readTemp = false;
         }
